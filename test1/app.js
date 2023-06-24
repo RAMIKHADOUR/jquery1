aafbcc308d26af2abfd006ce38864114e5891ed8
Rami KHADDOUR <ramikhaddour@gmail.com>
2023-06-24 18:13:14 +0200

    test
    jQuery

---
+++ test1/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+    function add(){
+$(".copied").addClass("bounce-effect");
+    }
+    function remove(){
+        $(".copied").removeClass("bounce-effect");
+            }
+            $(".copy-btn").click(function(){
+                $("#textField").select();
+                document.execCommand("copy");
+                add();
+                setTimeout(remove,800)
+            })
+})
